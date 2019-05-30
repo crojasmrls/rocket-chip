fdc8cd9f827624e69a56ffb2eadf810c20daf76b
carlos <crojasmrls@gmail.com>
2019-05-30 14:53:23 +0200

    Add compilation script for al Configs

---
+++ emulator/compile.sh
@@ -0,0 +1,11 @@
+for i in 2 4 6
+do
+    for j in MESI MSI MI
+    do 
+    	PART=$j$i
+    	CONSTANT=Config
+    	CONF=$PART$CONSTANT
+    	echo "$CONF"
+    	make CONFIG=$CONF debug
+    done
+done
